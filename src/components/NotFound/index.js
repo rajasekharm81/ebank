195b86ad2ee20e2a592dc41e9e6a1fe4b3cdb9be
Raja sekhar <rajasekhar.mandula@gmail.com>
2022-10-01 15:24:23 +0000

    all ui set

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,17 @@
 import {Component} from 'react'
+import './index.css'
 
 class NotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>NotFound</h1>
+      <div className="NotFoundContainer">
+        <img
+          className="notFoundImage"
+          src="https://assets.ccbp.in/frontend/react-js/ebank-not-found-img.png"
+          alt="not found"
+        />
+        <h1>Page Not Found</h1>
+        <p>We are sorry, the page you requested could not be found</p>
       </div>
     )
   }
