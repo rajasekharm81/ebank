ac819d1c71a44aa7083c58f2b2c107bba3a358d4
Raja sekhar <rajasekhar.mandula@gmail.com>
2022-10-01 15:14:54 +0000

    home ui set

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,27 @@
 import {Component} from 'react'
+import './index.css'
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="homeMainContainer">
+        <div className="headerContainer">
+          <img
+            className="websiteLogo"
+            src="https://assets.ccbp.in/frontend/react-js/ebank-logo-img.png"
+            alt="website logo"
+          />
+          <button className="logoutButton" type="button">
+            Logout
+          </button>
+        </div>
+        <div className="contentContainer">
+          <h1 className="ContentHeading">Your Flexibility Our Excellence</h1>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/ebank-digital-card-img.png"
+            alt="digital card"
+          />
+        </div>
       </div>
     )
   }
